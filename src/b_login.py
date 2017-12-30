c59cef9f0ef8249629921d17153161ea72be3c92
tbolp <tbolpsin@gmail.com>
2017-12-30 10:20:28 +0800

    b_login struct change

--- src/b_login.py
+++ src/b_login.py
@@ -1,70 +1,31 @@
 import pyqrcode
 import requests
-import pickle
 import json
 import time
+import http.cookiejar
 from b_capture import *
 
+
 def save_cookie(c, fn):
-    with open(fn, "wb") as f:
-        pickle.dump(requests.utils.dict_from_cookiejar(c), f)
+    with open(fn, "w") as f:
+        d = requests.utils.dict_from_cookiejar(c)
+        json.dump(d, f)
+
 
 def load_cookie(fn):
-    with open(fn, "rb") as f:
-        return requests.utils.cookiejar_from_dict(pickle.load(f))
+    with open(fn, "r") as f:
+        d = json.load(f)
+        return requests.utils.cookiejar_from_dict(d)
+
 
 def get_time():
     return int(round((time.time()*1000)))
 
-class Login(object):
 
-    def __init__(self):
-        self._session = requests.session()
-        self._cookie_fn = "cookie"
-        self._capture = Capture()
+class TaskReward(object):
 
-    def __enter__(self):
-        return self
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        save_cookie(self._session.cookies, self._cookie_fn)
-        pass
-
-    def is_login(self):
-        session = self._session
-        try:
-            session.cookies = load_cookie(self._cookie_fn)
-            resp = session.get("https://api.live.bilibili.com/FreeSilver/getCurrentTask")
-            info = json.loads(resp.text)
-            if info["code"] == -101:
-                return False
-            else:
-                return True
-        except FileNotFoundError:
-            return False
-        except:
-            return False
-
-    @classmethod
-    def login(cls):
-        login = Login()
-        key = login._get_qrcode("二维码已被保存到 QRCode.png,Ctrl+C 可退出")
-        session = login._session
-        try:
-            while True:
-                url = "https://passport.bilibili.com/qrcode/getLoginInfo"
-                data = {"oauthKey": key,
-                        "gourl": "https://www.bilibili.com/"}
-                r = session.post(url, data=data)
-                info = json.loads(r.text)
-                if info["status"]:
-                    print("登录成功")
-                elif info["data"] == -2:
-                    key = login._get_qrcode("二维码已更新,Ctrl+C 可退出")
-                time.sleep(5)
-        except KeyboardInterrupt:
-            print("再见")
-        return login
+    def __init__(self, login):
+        self._session = login.session
 
     def get_reward(self):
         session = self._session
@@ -117,6 +78,65 @@ class Login(object):
         print("验证码已保存到 capture.jpeg")
         return input("输入验证码: ")
 
+
+class Login(object):
+
+    @property
+    def session(self):
+        return self._session
+
+    def __init__(self, cookie_fn = "cookie"):
+        self._session = requests.session()
+        self._cookie_fn = cookie_fn
+        try:
+            self._session.cookies = load_cookie(cookie_fn)
+        except:
+            self._session.cookies = http.cookiejar.CookieJar()
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        save_cookie(self._session.cookies, self._cookie_fn)
+
+    def is_login(self):
+        session = self._session
+        try:
+            resp = session.get("https://api.live.bilibili.com/FreeSilver/getCurrentTask")
+            info = json.loads(resp.text)
+            if info["code"] == -101:
+                return False
+            else:
+                return True
+        except FileNotFoundError:
+            return False
+        except:
+            return False
+
+    @classmethod
+    def login(cls, cookie_fn = "cookie"):
+        login = Login(cookie_fn=cookie_fn)
+        if login.is_login():
+            return login
+        key = login._get_qrcode("二维码已被保存到 QRCode.png")
+        session = login.session
+        try:
+            while True:
+                url = "https://passport.bilibili.com/qrcode/getLoginInfo"
+                data = {"oauthKey": key,
+                        "gourl": "https://www.bilibili.com/"}
+                r = session.post(url, data=data)
+                info = json.loads(r.text)
+                if info["status"]:
+                    print("登录成功")
+                    return login
+                elif info["data"] == -2:
+                    key = login._get_qrcode("二维码已更新,Ctrl+C 可退出")
+                time.sleep(5)
+        except KeyboardInterrupt:
+            print("再见")
+        return None
+
     def _get_qrcode(self, msg = ""):
         resp = self._session.get("https://passport.bilibili.com/qrcode/getLoginUrl")
         info = json.loads(resp.text)
@@ -129,10 +149,6 @@ class Login(object):
             print(msg)
         return key
 
-l = Login()
-if l.is_login():
-    l.get_reward()
-else:
-    with Login.login() as l:
-        if l.login():
-            l.get_reward()
+with Login.login() as l:
+    task = TaskReward(l)
+    task.get_reward()
